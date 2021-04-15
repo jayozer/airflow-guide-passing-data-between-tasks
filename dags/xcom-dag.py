6030f28287810a690988b0c26a4d97073adc2805
jayozer <jozer@statestitle.com>
2021-04-14 19:02:49 -0700

    thrid commit

--- dags/xcom-dag.py
+++ dags/xcom-dag.py
@@ -14,14 +14,22 @@ def get_testing_increase(state, ti):
     """
     res = requests.get(url+'{0}/current.json'.format(state))
     testing_increase = json.loads(res.text)['totalTestResultsIncrease']
-
+    # xcom_push() method to specify a key name, 
     ti.xcom_push(key='testing_increase', value=testing_increase) 
+    
+    # alternatively we could have returned testing_increase
+    # return testing_increase
+    # ti.xcom_push(key='returned_value', value=testing_increase) 
+    
 
 
 def analyze_testing_increases(state, ti):
     """
     Evaluates testing increase results 
     """
+    # xcom pull method. Soecify "key" and task_ids associated with
+    # xcom we want to retrieve. Any xcom or even multiple xcoms is possible
+    # it does not need to be from a task immediately prior
     testing_increases=ti.xcom_pull(key='testing_increase', task_ids='get_testing_increase_data_{0}'.format(state))
     print('Testing increases for {0}:'.format(state), testing_increases)
     #run some analysis here
@@ -37,6 +45,8 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
+#current day covid increase data
+# with is the within a context manager
 with DAG('xcom_dag',
          start_date=datetime(2021, 1, 1),
          max_active_runs=2,
@@ -44,7 +54,7 @@ with DAG('xcom_dag',
          default_args=default_args,
          catchup=False
          ) as dag:
-
+    # This code is running inside a context
     opr_get_covid_data = PythonOperator(
         task_id = 'get_testing_increase_data_{0}'.format(state),
         python_callable=get_testing_increase,
